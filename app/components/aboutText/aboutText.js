436f093892a86034e59919ab16e545321e013424
melanie <melanieboeckmann@Melanies-MacBook-Pro.local>
2016-05-09 16:48:49 -0700

    Changed about text

--- app/components/aboutText/aboutText.js
+++ app/components/aboutText/aboutText.js
@@ -6,9 +6,9 @@ export default class AboutText extends React.Component {
 	render() {
 		return (
 			<ul className={styles.aboutText}>
-				<li>I love building and rebuilding websites.</li>
+				<li>I love being challenged and learning new things.</li>
 				<li>My name is Melanie,<br/>I am a Front-End Developer based in Vancouver, BC.</li>
-				<li>I love building and rebuilding websites.</li>
+				<li>I have over 2 years of web development experience.</li>
 			</ul>
 		);
 	}
